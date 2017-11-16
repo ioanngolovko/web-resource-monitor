96437f995c13891609e29af623aa06db3886e7d0
inponomarev <iponomarev@mail.ru>
2017-11-16 21:04:52 +0300

    assembly plugin instead of shade

--- src/main/java/ru/curs/wpm/Main.java
+++ src/main/java/ru/curs/wpm/Main.java
@@ -38,7 +38,7 @@ public class Main {
 
 
     public static void main(String args[]) {
-
+        System.out.println("Hello, world");
 
         WebResourcesMonitor webResourcesMonitor = new WebResourcesMonitor(6);
 
